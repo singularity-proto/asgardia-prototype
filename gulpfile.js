1addf85e406ca125156362a950181a33d2e990f7
Nihylum <nihylum@gmail.com>
2017-01-13 19:49:42 +0100

    +jquery

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,11 @@ var s_build = require('./semantic/tasks/build');
 
 gulp.task('watch-ui', s_watch);
 gulp.task('build-ui', s_build);
+
+gulp.task('copy-jquery', function() {
+   return gulp.src('./node_modules/jquery/dist/jquery.min.*')
+    .pipe(gulp.dest('./assets/libs/'));
+});
+
+gulp.task('build', ['build-ui', 'copy-jquery']);
+gulp.task('watch', 'watch-ui');
